a7dee2551228d232cb081d64acd7b6d94e5a9554
Shailaja1234 <81084379+Shailaja1234@users.noreply.github.com>
2021-10-13 22:43:29 +0530

    Update store.js
    Ok

--- store.js
+++ store.js
@@ -1,14 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './UserSlice';
+
 import adminReducer from './AdminSlice';
-import weatherReducer from './WeatherSlice';
+
 console.log('0 store');
 
 const store = configureStore({
     reducer: {
         user: userReducer,
-        admin: adminReducer,
-        weather:weatherReducer
+        admin: adminReducer
+      
     },
 })
 export default store;
